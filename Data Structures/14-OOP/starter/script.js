b0e4a8bfae424572c3c3df76fbe125e2094c5ea0
Nitish Kumar Yadav <engineer.nitish7115@gmail.com>
2022-11-16 19:36:26 +0530

    Rendering Marker and List

--- Data Structures/14-OOP/starter/script.js
+++ Data Structures/14-OOP/starter/script.js
@@ -8,7 +8,11 @@ Person.prototype.random = function () {
   console.log(this);
 };
 
-const jonas = new Person('Jonas', 22);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.age);
+};
+
+const jonas = new Person('Jonas', 1980);
 console.log(jonas);
 // Inherited from "Object"
 console.log(jonas.hasOwnProperty('firstName'));
@@ -62,6 +66,7 @@ console.log(`First Name Before Setting: ${nitish.firstName}`);
 nitish.firstName = 'Nitish Kumar Yadav';
 console.log(`First Name After Setting: ${nitish.firstName}`);
 
+// ***************************************************************************************************
 // *************************Object.create()**************************************
 
 const PersonProto = {
@@ -80,6 +85,7 @@ nitish2.init('Nitish', 1999);
 console.log(`Using Object.create(): ${nitish2.calcAge()}`);
 console.log(nitish2);
 
+// ****************************************************************************************************
 //*********************Coding Challenge 2 & 3 *********************************
 
 //*******************Challenge 2 **************************
@@ -117,7 +123,7 @@ porshe.brake();
 console.log(porshe.speedUS);
 
 //***************Challenge 3 ************************
-
+// *** Example of Inheritance of Constructors using Object.create and Parent.call(this,x,y...) method***
 const ElectricCar = function (make, speed, charge) {
   Car.call(this, make, speed);
   this.charge = charge;
@@ -140,3 +146,120 @@ ElectricCar.prototype.constructor = ElectricCar;
 const tesla = new ElectricCar('Tesla', 120, 50);
 tesla.chargeBattery(100);
 tesla.accelerate();
+
+// *****************************************************************************************************
+//***** Inheritance b/w ES6 Classes Using extend keyword and super() *****
+// **** Using Humans Class as Parent for Inheritance ****
+
+class student extends Humans {
+  constructor(firstName, age, course) {
+    super(firstName, age);
+    this.course = course;
+  }
+
+  intro() {
+    console.log(
+      `Hi!!! I am ${this._firstName} and I am doing Bachelors in ${this.course}`
+    );
+  }
+}
+
+const kamini = new student('Kamini Yadav', 18, 'Medical');
+kamini.calcAge();
+kamini.intro();
+
+// Special Case -> When there is no additional Parameter in child Class
+class studentSC extends Humans {
+  intro() {
+    console.log(`Hi!!! I am ${this._firstName} and I am doing Bachelors.`);
+  }
+}
+
+const shivam = new studentSC('Shivam Kumar', 1999);
+shivam.calcAge();
+shivam.intro();
+
+// ************************************************************************************************
+// ****** Implementing Inheritance Using Object.create() ******
+// Using PersonProto Object From Top
+
+const StudentOc = Object.create(PersonProto);
+StudentOc.init = function (firstName, age, course) {
+  PersonProto.init.call(this, firstName, age);
+  this.course = course;
+};
+StudentOc.intro = function () {
+  console.log(
+    `Hi!!! I am ${this.firstName} and I am doing Bachelors in ${this.course}`
+  );
+};
+
+const nitishOc = Object.create(StudentOc);
+nitishOc.init('Nitish', 1999, 'Computer Science');
+nitishOc.intro();
+
+// ********************************** Coding Challenge *************************************************
+//        ***** Implementing Inheritance Using Classes, Private variables and method chaining *****
+
+class carCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed = this.speed + 10;
+    console.log(`Accelerated Speed: ${this.speed}`);
+  }
+
+  brake() {
+    this.speed = this.speed - 5;
+    console.log(`Reduced Speed: ${this.speed}`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(value) {
+    this.speed *= 1.6;
+  }
+}
+
+class EvCl extends carCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 1;
+    console.log(
+      `${this.make} is going at ${this.speed} km/hr, with a charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+}
+
+const rivian = new EvCl('Rivian', 120, 23);
+
+rivian
+  .brake()
+  .accelerate()
+  .brake()
+  .brake()
+  .chargeBattery(50)
+  .chargeBattery(55)
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .chargeBattery(80)
+  .brake();
